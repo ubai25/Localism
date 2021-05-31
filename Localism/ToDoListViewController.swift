5aa7f50593c1c51230914c1daf68a5f1c212c1cb
Ubaidillah Ahmad <a.ubai25@gmail.com>
2021-05-31 11:01:48 +0700

    initial commit

---
+++ Localism/ToDoListViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Localism
+//
+//  Created by Ahmad Ubaidillah on 30/05/21.
+//
+
+import UIKit
+
+class ToDoListViewController : UITableViewController {
+    
+    let dummyArray = ["sholat shubuh", "olahraga", "sarapan", "mandi"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        dummyArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
+        cell.textLabel?.text = dummyArray[indexPath.row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
